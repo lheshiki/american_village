b9fc965d033acc4b26964183bb568a7bba1b9d5e
yokoyama yutaka <junsyuu2july30@ezweb.ne.jp>
2020-12-08 14:48:09 +0900

    no message

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+$('article')
+.on('mouseover',function(){
+	$(this).find(".blackLine").addClass("active");
+})
+.on("mouseleave",function(){
+	$(this).find(".blackLine").removeClass("active");
+});
+});

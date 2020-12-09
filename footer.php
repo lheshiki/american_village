10cfe71a7acb99a762a126a131daaee2fa003a16
Luis Heshiki <luisheshiki22@gmail.com>
2020-12-09 14:08:15 +0900

    Commit!

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer -->
+<footer class="justify-content-center">
+   
+  <!-- footer 中央 -->
+  <p class="footer_center">Copyright©️ Shinohara. ALL RIGHTS RESERVED.</p>
+</footer>
+</body>
+</html>

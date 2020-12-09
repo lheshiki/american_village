d873953551ecf84e1c6d9c60a0a0b24291913a7d
yokoyama yutaka <junsyuu2july30@ezweb.ne.jp>
2020-12-09 15:38:12 +0900

    no message

--- functions.php
+++ functions.php
@@ -1,3 +1,9 @@
 <?php
 add_theme_support('post-thumbnails');
+
+add_action( 'after_setup_theme', 'register_menu' );
+function register_menu() {
+  register_nav_menu( 'primary', __( 'Primary Menu', 'theme-slug' ) );
+}
 ?>
+
